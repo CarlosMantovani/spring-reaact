6e535b4a04b3186a641bda9cde8967736eeca1fb
Carlos Mantovani <mantovanicarloshenrique@gmail.com>
2024-11-06 14:09:55 -0300

    feature: criando dto para as imagens que irao ser requisitadas

---
+++ imageliteapi/src/main/java/mantovani/dev/imageliteapi/dto/ImageDTO.java
@@ -0,0 +1,15 @@
+package mantovani.dev.imageliteapi.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDate;
+@Data
+@Builder
+public class ImageDTO {
+    private String url;
+    private String name;
+    private String extension;
+    private Long size;
+    private LocalDate uploandDate;
+}
