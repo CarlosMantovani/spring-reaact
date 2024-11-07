1fbaa3f94acdd386349fed9ceb3e1958beac6b60
Carlos Mantovani <mantovanicarloshenrique@gmail.com>
2024-11-07 16:33:28 -0300

    feature: criando comunicação entre o front-end e o back-end

---
+++ imagelite/src/resources/image/image-service.ts
@@ -0,0 +1,12 @@
+import{ Image } from './image.resource'
+
+class ImageService{
+    basURL: string = 'http://localhost:8080/v1/images';
+
+    async buscar() : Promise<Image[]>{
+        const response = await fetch(this.basURL);
+        return await response.json();
+    }
+}
+
+export const useImageService = () => new ImageService();
