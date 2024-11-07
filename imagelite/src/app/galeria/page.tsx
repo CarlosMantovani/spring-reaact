b29bc9744f7355f3ec4b7b093483d8911d8f644d
Carlos Mantovani <mantovanicarloshenrique@gmail.com>
2024-11-07 15:56:12 -0300

    feature: Criando componente ImageCard e pagina da galeria

--- imagelite/src/app/galeria/page.tsx
+++ imagelite/src/app/galeria/page.tsx
@@ -1,10 +1,18 @@
-import { Template } from '@/components'
+import { Template, ImageCard } from '@/components'
 
 
 export default function GaleriaPage(){
     return(
         <Template>
-            <h1>Galeria</h1>
+
+            <section className="grid grid-cols-3 gap-8">
+            <ImageCard nome="Lago" tamanho="10MB" dataUpload="01/01/2024"src='https://plus.unsplash.com/premium_photo-1673697239936-c2599b0b0498?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cGFpc2FnZW0lMjBuYXR1cmV6YSUyMHdhbGxwYXBlcnN8ZW58MHx8MHx8fDA%3D'/>
+            <ImageCard nome="Lago" tamanho="10MB" dataUpload="01/01/2024"src='https://plus.unsplash.com/premium_photo-1673697239936-c2599b0b0498?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cGFpc2FnZW0lMjBuYXR1cmV6YSUyMHdhbGxwYXBlcnN8ZW58MHx8MHx8fDA%3D'/>
+            <ImageCard nome="Lago" tamanho="10MB" dataUpload="01/01/2024"src='https://plus.unsplash.com/premium_photo-1673697239936-c2599b0b0498?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cGFpc2FnZW0lMjBuYXR1cmV6YSUyMHdhbGxwYXBlcnN8ZW58MHx8MHx8fDA%3D'/>
+            <ImageCard nome="Lago" tamanho="10MB" dataUpload="01/01/2024"src='https://plus.unsplash.com/premium_photo-1673697239936-c2599b0b0498?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cGFpc2FnZW0lMjBuYXR1cmV6YSUyMHdhbGxwYXBlcnN8ZW58MHx8MHx8fDA%3D'/>
+            <ImageCard nome="Lago" tamanho="10MB" dataUpload="01/01/2024"src='https://plus.unsplash.com/premium_photo-1673697239936-c2599b0b0498?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cGFpc2FnZW0lMjBuYXR1cmV6YSUyMHdhbGxwYXBlcnN8ZW58MHx8MHx8fDA%3D'/>
+            </section>
+            
         </Template>
     )
 }
