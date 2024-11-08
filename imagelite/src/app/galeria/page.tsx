c93f39ec12c298cd3db1d95104b0bf16ac6e2f45
Carlos Mantovani <mantovanicarloshenrique@gmail.com>
2024-11-08 09:23:51 -0300

    feature: Criando formulario para pesquisa de imagem

--- imagelite/src/app/galeria/page.tsx
+++ imagelite/src/app/galeria/page.tsx
@@ -28,7 +28,17 @@ export default function GaleriaPage(){
     return(
         
         <Template>
-        <button className='bg-gray-500' onClick={searchImage}>Clique para mudar</button>
+            <section className='flex flex-col items-center justify-center my-5'>
+                <div className='flex space-x-4'>
+                    <input type='text' className='border px-3 py-2 rounded-lg text-gray-900'/>
+                        <select className='border px-4 py-2 rounded-lg text-gray-900'>
+                            <option >All formats</option>
+                        </select>
+                        <button className='bg-blue-500 text-whit px-4 py-2 rounded-lg' onClick={searchImage} >Search</button>
+                        <button className='bg-yellow-500 text-whit px-4 py-2 rounded-lg'>Add New
+                        </button>
+                </div>
+            </section>
             <section className="grid grid-cols-3 gap-8">
 
             {
