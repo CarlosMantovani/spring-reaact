eedaa9e6c41cd3b2c9dcbe43f66f3547e1b62fb1
Carlos Mantovani <mantovanicarloshenrique@gmail.com>
2024-11-08 13:17:28 -0300

    feature: Criando pagina de formulario para nova postagem

---
+++ imagelite/src/app/formulario/page.tsx
@@ -0,0 +1,10 @@
+import { Template } from "@/components"
+
+export default function FormularioPage(){
+    return(
+        <Template>
+            <h1 className="text-gray-900">Formulario</h1>
+        </Template>
+        
+    )
+}
