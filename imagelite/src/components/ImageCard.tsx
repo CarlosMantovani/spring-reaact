6ba2ae04840ee5124b6ac4ed1943531d773afa0a
Carlos Mantovani <mantovanicarloshenrique@gmail.com>
2024-11-08 10:16:55 -0300

    feature: Criando função de baixar a imagem

--- imagelite/src/components/ImageCard.tsx
+++ imagelite/src/components/ImageCard.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 interface ImageCardProps{
     nome?: string;
     tamanho?: string;
@@ -6,8 +8,13 @@ interface ImageCardProps{
 }
 
 export const ImageCard: React.FC<ImageCardProps> = ({nome, dataUpload,src,tamanho}: ImageCardProps) => {
+    
+    function download(){
+        window.open(src, '_blank')
+    }
+
     return(
-        <div className="card relative bg-white rounded-md shadow-md transition-tranform ease-in duration-300 transform hover:shadow-lg hover:-translate-y-2">
+        <div onClick={download} className="card relative bg-white rounded-md shadow-md transition-tranform ease-in duration-300 transform hover:shadow-lg hover:-translate-y-2">
             <img src={src} className="h-56 w-full object-cover rounded-t-md"  alt="" />
             <div className="card-body p-4">
                 <h5 className="text-xl font-semibold mb-2 text-gray-600">{nome}</h5>
